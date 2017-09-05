76ff377587e840779f449ab16afbd2987dc4c355
Vlad <vlad.filip.dev@gmail.com>
2017-09-05 12:12:30 +0300

    updated datetime

--- utils/datetime.js
+++ utils/datetime.js
@@ -6,18 +6,12 @@ class DateTime extends Date {
     return this.toISOString().slice( 0, 19 ).replace( 'T', ' ' )
   }
 
-  static diff ( start, end ) {
-    let duration = end.getTime() - start.getTime()
-    let milliseconds = parseInt( ( duration % 1000 ) / 100 )
-    let seconds = parseInt( ( duration / 1000 ) % 60 )
-    let minutes = parseInt( ( duration / ( 1000 * 60 ) ) % 60 )
-    let hours = parseInt( ( duration / ( 1000 * 60 * 60 ) ) % 24 )
+  toTime () {
+    return this.toISOString().slice( 11, 23 )
+  }
 
-    hours = ( hours < 10 ) ? '0' + hours : hours
-    minutes = ( minutes < 10 ) ? '0' + minutes : minutes
-    seconds = ( seconds < 10 ) ? '0' + seconds : seconds
-
-    return hours + ':' + minutes + ':' + seconds + '.' + milliseconds
+  toDate () {
+    return this.toISOString().slice( 0, 10 )
   }
 
 }
